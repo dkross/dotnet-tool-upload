63e86888a9fb55feb79b5a24d9dad7fab7f72750
Daniel Kraemer <dkraemer@dkross.org>
2023-09-23 04:09:44 +0200

    Initial commit

---
+++ src/Upload/projectref/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DKrOSS.DotNet.Tools.Upload;
+
+file static class Program
+{
+#pragma warning disable CS1998
+    private static async Task Main(string[] args)
+#pragma warning restore CS1998
+    {
+
+    }
+}
